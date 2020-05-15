acf06993ff5b442e1a04d460288b1ec6adaaf37e
Kyle Alwyn <kjalwyn@gmail.com>
2020-05-15 12:06:19 -0700

    Update cli/commands/clone.ts
    Co-authored-by: Erik Shestopal <6404663+erikshestopal@users.noreply.github.com>

--- cli/commands/clone.ts
+++ cli/commands/clone.ts
@@ -3,5 +3,5 @@ import * as adapters from '../adapters';
 
 export default async function clone({ adapter, path }: { adapter: 'contentful'; path: string }) {
   const data = await adapters[adapter]();
-  writeFileSync(path, `export const copy = ${JSON.stringify(data, null, 2)};`);
+  writeFileSync(path, `export const copy = ${JSON.stringify(data, null, 2)} as const;`);
 }

01650eaa913c2fdac601472178ac29b983c0860b
Kyle Alwyn <kjalwyn@gmail.com>
2020-05-15 00:45:05 -0700

    Sync prettier

--- src/helpers.ts
+++ src/helpers.ts
@@ -5,7 +5,7 @@ export const getInterpolatedCopy = <TCopy, TContext>(copy: TCopy, context: TCont
 };
 
 const findInterpolations = (copy: any, context: any) => {
-  Object.keys(copy).forEach((key) => {
+  Object.keys(copy).forEach(key => {
     const currItem = copy[key];
     if (typeof currItem === 'object') {
       findInterpolations(currItem, context);
@@ -17,10 +17,12 @@ const findInterpolations = (copy: any, context: any) => {
 
 const interpolateCopy = (str: string, context: any) => {
   const delimeterMatcher = /{\S+}/gm;
-  const variables = (str.match(delimeterMatcher) || []).map((dV: string) => dV.substring(1, dV.length - 1));
+  const variables = (str.match(delimeterMatcher) || []).map((dV: string) =>
+    dV.substring(1, dV.length - 1)
+  );
   if (variables.length) {
     let interpolatedValue = str;
-    variables.forEach((v) => {
+    variables.forEach(v => {
       const target = `{${v}}`;
       const value = context[v] || target;
       interpolatedValue = interpolatedValue.replace(target, value);

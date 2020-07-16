c5cac1b32433583556976d9655c6a2e235978ab5
Gerrymi Bernardo <gerrymibernardo@gmail.com>
2020-07-16 09:37:38 -0700

    fix: restore overriding provider value through copy prop (#7)
    * fix: restore overriding provider value through copy prop
    
    * feat: remove state from provider

--- src/copyful.tsx
+++ src/copyful.tsx
@@ -4,15 +4,15 @@ import { getInterpolatedCopy } from './helpers';
 type InterpolationContext = Record<string, string | number>;
 
 export const createCopyful = <TCopy extends object>(defaultCopy: TCopy) => {
-  const Context = React.createContext(defaultCopy);
+  const Context = React.createContext<Partial<TCopy>>(defaultCopy);
 
-  const CopyfulProvider: React.FC = ({ children }) => (
-    <Context.Provider value={defaultCopy}>{children}</Context.Provider>
+  const CopyfulProvider: React.FC<{ copy?: Partial<TCopy> }> = ({ children, copy }) => (
+    <Context.Provider value={copy || defaultCopy}>{children}</Context.Provider>
   );
 
   const useCopy = <T extends keyof TCopy>(key: T, context: InterpolationContext = {}) => {
     const copy = React.useContext(Context);
-    const copyBlock = copy[key];
+    const copyBlock = copy[key] || defaultCopy[key];
 
     const interpolatedCopy: TCopy[T] = context
       ? getInterpolatedCopy(copyBlock, context)
